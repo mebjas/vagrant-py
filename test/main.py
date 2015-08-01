7a316f1e02c72b919f6bb82e5db226e69191d0bb
Minhaz A V <minhazav@gmail.com>
2015-08-02 01:26:04 +0530

    test cases added to start daemon, stop, and test pipes

--- test/main.py
+++ test/main.py
@@ -1,42 +1,93 @@
-#!/usr/bin/env python
+import unittest
+import os
+import sys
+import inspect
+import shutil
 
-# TODO: change this with proper unit testing
-# Code sample to test communication with daemon
-# Via two named pipes
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 
-import os, time, sys
+import daemon
+import subprocess
+import time
+import json
 
-start = time.time()
-
-#Make a connection to the daemon
-print "init test process..."
-randStr = "qwerty122"
-o_pipename = "../tmp/pipe"
-i_pipename = "../tmp/" +randStr
-xmlfilepath = "/Users/minhazav/github/vagrant-pyd/test/sample.xml"
-
-print "conecting to daemon via named pipe..."
-outfifo = open(o_pipename, 'w+')
-
-print "sending commadn to daemon...."
-command = randStr +" create " +xmlfilepath +" "
-outfifo.write(command)
-outfifo.close()
-print "command sent...."
-
-print "creating listener pipe...."
-# Now listen to a specific pipe
-if not os.path.exists(i_pipename):
-	os.mkfifo(i_pipename)
+# Global funcitions needed for testing
 
 
-print "waiting for daemon to respond..."
-i_fifo = open(i_pipename, 'r')
-while True:
-	line = i_fifo.readline()[:-1]
-	if line:
-		print '[%s] Output Recieved: %s' % (time.time(), line)
-		os.unlink(i_pipename)
-		break
+def isProcessRunning(pid):
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        return False
+    else:
+        return True
 
-print "time taken: %s" % (time.time() - start)
+class TestDaemon(unittest.TestCase):
+
+    def setUp(self):
+        # Start the daemon
+        self.currentdir = os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe())))
+        self.parentdir = os.path.dirname(self.currentdir)
+
+        print "\n---- Starting Vagrant Daemon ----"
+        subprocess.call([self.parentdir +'/main.py', 'start'])
+
+    def tearDown(self):
+        # Stop the Daemon
+        print "\n---- Stopping Vagrant Daemon ----"
+        subprocess.call([self.parentdir +'/main.py', 'stop'])
+
+
+        # Do clean up tasks
+        if os.path.exists(self.currentdir + "/tmp"):
+            shutil.rmtree(self.currentdir + "/tmp")
+
+    def test_daemon_active(self):
+        # Get Process ID from ../tmp/process.pid file
+        with open("../tmp/process.pid", 'r') as pf:
+            pid = int(pf.read().strip())
+            self.assertTrue(isProcessRunning(pid))
+
+    def test_daemon_restart(self):
+        print "\n---- Restarting Vagrant Daemon ----"
+        subprocess.call([self.parentdir +'/main.py', 'restart'])
+        with open("../tmp/process.pid", 'r') as pf:
+            pid = int(pf.read().strip())
+            self.assertTrue(isProcessRunning(pid))
+
+    def test_communication(self):
+        # Send a command via pipe
+        # Make a connection to the daemon
+        randStr = "qwerty122"
+        o_pipename = "../tmp/pipe"
+        i_pipename = "../tmp/" +randStr
+        xmlfilepath = "/Users/minhazav/github/vagrant-pyd/test/sample.xml"
+        outfifo = open(o_pipename, 'w+')
+        command = randStr +" create " +xmlfilepath +" "
+        outfifo.write(command)
+        outfifo.close()
+
+        # Now listen to a specific pipe
+        if not os.path.exists(i_pipename):
+            os.mkfifo(i_pipename)
+        i_fifo = open(i_pipename, 'r')
+        while True:
+            line = i_fifo.readline()[:-1]
+            if line:
+                data = json.loads(line)
+
+                self.assertEquals('', data['message']);
+                self.assertFalse(data['error'])
+                
+                os.unlink(i_pipename)
+                break
+
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDaemon)
+    unittest.TextTestRunner(verbosity=2).run(suite)
